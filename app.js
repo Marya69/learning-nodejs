e3c3897f025b0329cd45cd096ebc8d6d8d90df5b
Marya <maryajabar63@gmail.com>
2025-09-01 17:41:26 +0300

    Add app.js

---
+++ app.js
@@ -0,0 +1,12 @@
+const number =13 ;
+
+if (number > 10) {
+    console.log("The number is greater than 10");
+}
+else{
+    console.log("The number is 10 or less");
+
+
+}
+
+console.log(" first file in learning nodejs step by step ");
